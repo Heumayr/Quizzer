acca473f00a6c255cc244d4aae08ede842f38614
Heumayr <80398233+Heumayr@users.noreply.github.com>
2026-03-15 20:03:28 +0100

    FH

---
+++ Quizzer.DataModels/Enumerations/BuzzerControlsLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizzer.DataModels.Enumerations
+{
+    public enum BuzzerControlsLayout
+    {
+        Buzzer = 0,
+        MulitpleChoice = 1,
+        Input = 2,
+    }
+}
